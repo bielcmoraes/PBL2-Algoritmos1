99174c3002da765a6f5fff0e6d1c5b38619da5ca
Gabriel <gcmorais66@gmail.com>
2021-04-14 18:31:24 -0300

    Modularização e Colisão do Tiro
    Cada objeto na tela foi separado em uma função e a colisão do tiro com a nave foi implementada. Kappa

--- game.py
+++ game.py
@@ -7,30 +7,52 @@ game_on = True
 linhas = 40
 colunas = 65
 linha_inicio_meteoro = 0
-posicao_inicial_meteoro = posicao_aleatoria_meteoro(colunas)
 centro_da_nave = (colunas // 2) + 1
 linha_inicio_tiro = linhas - 1
 
 while game_on:
     if not keyboard.is_pressed('esc'):
+        posicao_inicial_meteoro = posicao_aleatoria_meteoro(colunas)
         
         for movimento in range(0, linhas-4):
             clock = movimento % 5 #Clock em 5 que garante que o laço só seja executado nos ranges multipos de 5
+
+            matriz_vazia = gera_matriz(linhas, colunas) #Gera matriz mazia
     
-            centro_da_nave = movimento_nave(centro_da_nave)
+            centro_da_nave = movimento_nave(centro_da_nave) #retorna a posicao da nave a cada mudança
 
             linha_inicio_tiro = movimento_nave(centro_da_nave)
 
-            tiro = projetil()
+            controle_nave = movimento_nave(centro_da_nave) #Controla a posição da nave
 
-            tela_atual = meteoros(linhas, colunas, posicao_inicial_meteoro, linha_inicio_meteoro,centro_da_nave,linha_inicio_tiro, tiro)
+            tela_nave = nave(linhas, colunas, matriz_vazia, centro_da_nave) # Gera a nave na tela
 
-            linha_inicio_meteoro = movimento
-        
-            linha_inicio_tiro += movimento
-            if clock != 0:
-                os.system('cls')
-                atualiza_tela(tela_atual)
+            controle_tiro = projetil() #Controla o tiro
+
+            controle_colisao = colisao(controle_tiro, posicao_inicial_meteoro, centro_da_nave)
+
+            ultima_tela = tiro(linhas, colunas, tela_nave, controle_tiro, centro_da_nave) #Gera o tiro na tela
+
+            tela_meteoro = meteoros(linhas, colunas, tela_nave, posicao_inicial_meteoro, linha_inicio_meteoro, controle_colisao) #Gera o meteoro na tela
+            
+            controle_colisao = colisao(controle_tiro, posicao_inicial_meteoro, centro_da_nave)
+
+            if controle_colisao == True:
+                if clock != 0:
+                    os.system('cls')
+                atualiza_tela(tela_nave) #Imprime a ultima tela
                 sleep(0.3)
+                break
+            
+            else:
+
+                linha_inicio_meteoro = movimento
+            
+                linha_inicio_tiro += movimento
+                
+                if clock != 0:
+                    os.system('cls')
+                    atualiza_tela(ultima_tela) #Imprime a ultima tela
+                    sleep(0.3)
     else:
         game_on = False 
--- tela.py
+++ tela.py
@@ -1,9 +1,8 @@
 from random import randint
-import os
 from time import sleep
 import keyboard
 
-def meteoros(numero_linhas, numero_colunas, posicao, linha_referencia, centro_da_nave, referencia_tiro, projetil):
+def gera_matriz(numero_linhas, numero_colunas):
     tela = [] #Matriz da tela
     vazio = str(' ')
 
@@ -13,8 +12,38 @@ def meteoros(numero_linhas, numero_colunas, posicao, linha_referencia, centro_da
             linha.append(vazio) #elementos que formam as colunas da matriz
 
         tela.append(linha)
+    return tela
 
+#Gera a coluna em que o o meteoro aparece
+def posicao_aleatoria_meteoro(colunas):
+    posicao = randint(3, colunas-3) #Posição que o centro do meteoro aparece na tela
+    return posicao
+
+#Gera a nave na matriz
+def nave(numero_linhas, numero_colunas, matriz_vazia, centro_da_nave, ):
+    
+    tela = matriz_vazia
+
+    for i in range(0, 3):
+        if 3 <= centro_da_nave <= numero_colunas-3: # garante que a nave não saia do matriz
+            tela[numero_linhas - 1][centro_da_nave + i] = '█'
+            tela[numero_linhas - 1][centro_da_nave - i] = '█'
+            
+            if i < 2:
+                tela[numero_linhas - 2][centro_da_nave + i] = '█'
+                tela[numero_linhas - 2][centro_da_nave - i] = '█'
+            
+            if i < 1:
+                tela[numero_linhas - 3][centro_da_nave] = '█'
+    
+    return tela
+def meteoros(numero_linhas, numero_colunas, matriz_nave, posicao, linha_referencia, colisao):
+    
+    tela = matriz_nave
     coluna_referencia = posicao-1 #coluna utilizada para gerar a hitbox do meteoro
+    
+    if colisao == True:
+        linha_referencia = 0
     
     #Gera formato do meteoro
     for i in range(coluna_referencia,coluna_referencia + 3): #Laço que gera o retangulo do meteoro
@@ -29,34 +58,21 @@ def meteoros(numero_linhas, numero_colunas, posicao, linha_referencia, centro_da
     tela[2 + linha_referencia][coluna_referencia-2] = '▪'
     tela[2 + linha_referencia][coluna_referencia+4] = '▪'
     
-  
-    #Gera a nave na matriz
-    
-    for i in range(0, 3):
-        if 3 <= centro_da_nave <= numero_colunas-3: # garante que a nave não saia do matriz
-            tela[numero_linhas - 1][centro_da_nave + i] = '█'
-            tela[numero_linhas - 1][centro_da_nave - i] = '█'
-            
-            if i < 2:
-                tela[numero_linhas - 2][centro_da_nave + i] = '█'
-                tela[numero_linhas - 2][centro_da_nave - i] = '█'
-            
-            if i < 1:
-                tela[numero_linhas - 3][centro_da_nave] = '█'
-            
-    #Gera o tiro na matriz
-    for i in range(4, numero_linhas):
-        if 3 <= centro_da_nave <= numero_colunas-3:
-            if keyboard.is_pressed('space'):
-                tela[numero_linhas- i][centro_da_nave] = '|'
-    
-            
     return tela
 
+#Gera o tiro na matriz
+def tiro(numero_linhas, numero_colunas, matriz_meteoro, projetil, centro_da_nave):
 
-def posicao_aleatoria_meteoro(colunas):
-    posicao = randint(3, colunas-3) #Posição que o centro do meteoro aparece na tela
-    return posicao
+    tela = matriz_meteoro
+
+    if projetil == True:
+        for i in range(4, numero_linhas):
+            if 3 <= centro_da_nave <= numero_colunas-3:
+                if keyboard.is_pressed('space'):
+                    tela[numero_linhas- i][centro_da_nave] = '|'  
+        
+    return tela
+
 
 #printa a matriz que forma a tela
 def atualiza_tela(matriz_da_tela):
@@ -68,15 +84,29 @@ def atualiza_tela(matriz_da_tela):
 def movimento_nave(centro_da_nave):
 
     while keyboard.is_pressed('left'):
-        centro_da_nave -= 3
+        centro_da_nave -= 1
         return centro_da_nave
     
     while keyboard.is_pressed('right'):
-        centro_da_nave += 3
+        centro_da_nave += 1
         return centro_da_nave
-    
+
     return centro_da_nave
 
 def projetil():
-    if keyboard.is_pressed('space'):
+
+    while keyboard.is_pressed('space'):
         return True
+    
+    return False
+
+def colisao(tiro, coluna_referencia, centro_da_nave):
+    
+    #Colisao do tiro
+    if tiro == True:
+        for i in range (coluna_referencia - 2, coluna_referencia + 5): #verifica se o tiro acertou a hitbox do meteoro
+            if centro_da_nave == i:
+                
+                colisao_tiro = True
+                
+                return colisao_tiro
